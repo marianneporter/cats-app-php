bc60f27caa0f81a95bb6f585d8f6a27335a09d60
marianneporter <mariannneporter17@gmail.com>
2022-07-01 14:00:10 +0100

    register and login added to nav

--- includes/nav.php
+++ includes/nav.php
@@ -1,19 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Cats App PHP</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Home</a>
+          <a class="nav-link home-link" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="catslist.php">Cats List</a>
+          <a class="nav-link list-link" href="catslist.php">Cats List</a>
         </li> 
         <li class="nav-item">
-          <a class="nav-link" href="about.php" >About</a>
+          <a class="nav-link about-link" href="about.php" >About</a>
         </li>
       </ul>
       <ul class="d-flex navbar-nav mb-2 mb-lg-0">
@@ -24,24 +24,17 @@
   </div>
 </nav>
 
-<!-- <nav class="mt-5 cats-nav" >
-    <div class="nav-sm">
 
-    </div>
-    <div class="nav-left-lg d-sm-none">
-        <div class="site-name">Cats App PHP</div>
-        <ul class="main-nav">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="catslist.php">Cats List</a></li>
-            <li><a href="about.php">About</a></li>
-        </ul>
-    </div>
-   
-    <div class="nav-right">
-        <li><a href="register.php">Register</a></li>
-        <li><a href="login.php">Login</a></li>
-    </div>
+<script>
+    window.addEventListener('load', (event) => {
+        if (window.location.pathname.includes('catslist')) {
+            document.querySelector('.list-link').classList.add('highlighted');
+            console.log('added highlighted class to cats list');
+        } else if (window.location.pathname.includes('about')) {
+            document.querySelector('.about-link').classList.add('highlighted');
+        } else {
+            document.querySelector('.home-link').classList.add('highlighted');
+        }       
+    })
     
-</nav>
- -->
-
+</script>
