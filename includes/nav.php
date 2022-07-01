30c47aca6618b47386609f5f2f817155e869bf5c
marianneporter <mariannneporter17@gmail.com>
2022-07-01 12:30:10 +0100

    WIP new navbar

--- includes/nav.php
+++ includes/nav.php
@@ -1,19 +1,47 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Cats App PHP</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" href="index.php">Home</a>
-        <a class="nav-link" href="catslist.php">Cats List</a>
-        <a class="nav-link" href="about.php">About</a>
-      </div>
-    </div>
-    <div class="login-nav mr-2">
-          <a class="nav-link" href="register.php">Register</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="catslist.php">Cats List</a>
+        </li> 
+        <li class="nav-item">
+          <a class="nav-link" href="about.php" >About</a>
+        </li>
+      </ul>
+      <ul class="d-flex navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>
+          <li class="nav-item"><a class="nav-link" href="#" >Login</a><li>
+      </ul>
     </div>
   </div>
 </nav>
+
+<!-- <nav class="mt-5 cats-nav" >
+    <div class="nav-sm">
+
+    </div>
+    <div class="nav-left-lg d-sm-none">
+        <div class="site-name">Cats App PHP</div>
+        <ul class="main-nav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="catslist.php">Cats List</a></li>
+            <li><a href="about.php">About</a></li>
+        </ul>
+    </div>
+   
+    <div class="nav-right">
+        <li><a href="register.php">Register</a></li>
+        <li><a href="login.php">Login</a></li>
+    </div>
+    
+</nav>
+ -->
 
